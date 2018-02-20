0d9581f88dfb45d3f98cb25c38bf1b30751fcdfc
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-02-19 23:03:24 -0500

    User persistance

--- twitter_alamofire_demo/AppDelegate.swift
+++ twitter_alamofire_demo/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         if(User.current != nil){
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let homeTimelineViewController = storyboard.instantiateViewController(withIdentifier: "TimelineViewController")
+            let homeTimelineViewController = storyboard.instantiateViewController(withIdentifier: "TweetsNavigationController")
             window?.rootViewController = homeTimelineViewController
         }
         
